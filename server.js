1f08b9a78b2180aa297575c26e523c49613eb710
Nathan <67047470+nathnado22@users.noreply.github.com>
2022-10-19 16:25:37 +1100

    Working on Widget

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("<h3>Hello World!</h3>");
+});
+
+app.post("/webhooks/callback", async (req, res) => {
+  const { type } = req.body?.subscription ?? {};
+  const { event } = req.body;
+
+  console.log("request: ", req.body);
+  /* console.log(
+    `Receiving ${type} request for ${event.broadcaster_user_name}: `,
+    event
+  );*/
+
+  res.status(200).end();
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
